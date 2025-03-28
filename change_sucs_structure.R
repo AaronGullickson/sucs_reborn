dc9b519c2ea786eab6e3870121319798ee629582
Aaron Gullickson <aarongullickson@gmail.com>
2025-03-28 12:07:32 -0700

    Integrate mekhq id

--- change_sucs_structure.R
+++ change_sucs_structure.R
@@ -4,21 +4,25 @@ library(here)
 
 
 
-# Read in SUCS data -------------------------------------------------------
+# Read in data -------------------------------------------------------
 
 gs4_deauth()
 
 sucs_data <- read_sheet("1uO6aZ20rfEcAZJ-nDRhCnaNUiCPemuoOOd67Zqi1MVM", 
-                        sheet = "Systems", skip = 1) |>
-  select(-systemName, -alternateName, -x, -y, -size, -sarnaLink, -`distance (LY)`) |>
-  rename(sucsId = systemId)
+                        sheet = "Systems", skip = 1)
+
+id_crosswalk <- read_sheet("17GFFFp1sGvSYcs8DBryleQGvqgppX8EZ8MuF6Oq3or8")
 
 
 # Reshape and clean SUCS data ---------------------------------------------
 
+sucs_data <- sucs_data |>
+  select(-systemName, -alternateName, -x, -y, -size, -sarnaLink, -`distance (LY)`) |>
+  rename(id_sucs = systemId)
+
 # pivoting longer will get it in the format we want
 sucs_data <- sucs_data |>
-  pivot_longer(cols = !sucsId, names_to = "time_point", values_to = "faction") |>
+  pivot_longer(cols = !id_sucs, names_to = "time_point", values_to = "faction") |>
   # remove missing values in faction at this point
   filter(!is.na(faction))
 
@@ -35,22 +39,28 @@ sucs_data <- sucs_data |>
     faction = str_remove(faction, "\\s*\\([^\\)]+\\)")
   )
 
+# add in MekHQ ids
+sucs_data <- sucs_data |>
+  left_join(id_crosswalk) |>
+  select(id_sucs, id_mekhq, time_point, faction)
 
 # Add map source information ----------------------------------------------
 
-# we will start with the easy cases where we have a single map source for the
-# column
+# Lets start with the founding cases.
 test <- sucs_data |>
   mutate(
     source_type = "map",
     source_title = case_when(
-      time_point == "2271" ~ "Handbook: House Marik"
+      time_point == "2271" ~ "Handbook: House Marik",
+      time_point == "2317" ~ "Handbook: House Davion"
     ),
     source_loc = case_when(
-      time_point == "2271" ~ "p. 16"
+      time_point == "2271" ~ "p. 16",
+      time_point == "2317" ~ "p. 18"
     ),
     source_date = case_when(
-      time_point == "2271" ~ date("2271-06-01") # day before FWL
+      time_point == "2271" ~ date("2271-06-01"), # day before FWL founding
+      time_point == "2317" ~ date("2317-06-26")  # founding day for FS
     )
     
   )
