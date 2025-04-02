41fff1a14f1ca377a351ad814fe0a6e754f08ffd
Aaron Gullickson <aarongullickson@gmail.com>
2025-04-02 16:54:09 -0700

    Add era radio buttons

--- map_app.R
+++ map_app.R
@@ -19,10 +19,26 @@ load("sucs_data.RData")
 source_types <- unique(sucs_data$source_type)
 names(source_types) <- str_to_title(source_types)
 sources <- sort(unique(sucs_data$source_title))
-time_periods <- tribble(
-  ~period,  ~date,
-  "Free Worlds League Founding", date("2271-06-02"),
-  "Federated Suns Founding", date("2317-06-26"),
+time_periods <- list(
+  "Major Houses Founded (2366)" = "2366-12-31",
+  "Founding of the Star League (2750)" = "2571-12-31",
+  "End of Reunification War (2596)" = "2596-12-31",
+  "Height of the Star League (2750)" = "2750-12-31",
+  "Start of First Succession War (2864)" = "2786-01-01",
+  "End of First Succession War (2864)" = "2822-12-31",
+  "Start of Second Succession War (2864)" = "2830-01-01",
+  "End of Second Succession War (2864)" = "2864-12-31",
+  "End of Third Succession War (3025)" = "3025-12-31",
+  "End of Fourth Succession War (3030)" = "3030-01-31",
+  "End of War of 3039 (3040)" = "3040-01-31",
+  "Start of Clan Invasion (3049)" = "3049-06-01",
+  "End of Clan Invasion (3052)" = "3052-10-31",
+  "Start of FedCom Civil War (3063)" = "3063-12-31",
+  "End of FedCom Civil War (3067)" = "3067-10-31",
+  "Mid Jihad (3075)" = "3075-12-31",
+  "Late Jihad (3079)" = "3079-12-31",
+  "End of the Jihad (3081)" = "3081-12-31",
+  "Early Republic of the Sphere (3085)" = "3085-12-31"
 )
 
 # Define UI for application that draws a histogram
@@ -35,16 +51,23 @@ ui <- page_fillable(
     layout_sidebar(
       sidebar = sidebar(
         width = 350,
+        radioButtons( 
+          inputId = "era", 
+          label = h4("Pick an era..."), 
+          choices = time_periods,
+          selected = character(0)
+        ), 
         dateInput(
           inputId = "date",
-          label = "Choose a date",
+          label = h4("Or choose a specific date..."),
           value = date("3085-10-31")),
         selectInput( 
           "select_color", 
-          "Color by:", 
+          h4("Color by:"), 
           list("Faction" = "faction", "Source" = "source") 
         ),
         downloadButton("download", "Download CSV file"),
+        h4("Further Filters"),
         input_switch(
           "remove_undiscovered", 
           "Remove Undiscovered?", 
@@ -52,13 +75,13 @@ ui <- page_fillable(
         ), 
         checkboxGroupInput( 
           "source_types", 
-          "Source Types:", 
+          h5("Source Types:"), 
           source_types,
           selected = source_types
         ),
         checkboxGroupInput( 
           "sources", 
-          "Sources:", 
+          h5("Sources:"), 
           sources,
           selected = sources
         )
@@ -69,10 +92,15 @@ ui <- page_fillable(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
   # uncomment out to test out bootstrap themes
   #bs_themer()
+  
+  observeEvent(input$era,{
+    updateDateInput(session, "date", value = date(input$era))
+    
+  })
   
   output$plot <- renderPlotly({ 
     sucs_data |>
